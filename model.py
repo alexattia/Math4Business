064da22a6c4fc14453bbaeba28de0efb20eaa518
Alexandre Attia <alexattia18@gmail.com>
2017-12-13 18:48:12 +0100

    model from dumps in addition of parsers

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ import parserM4
 import pandas as pd
 import numpy as np
 import imp
+import pathlib
 columns = ['Height', 'Transactions', 'AvgFee/Tx', 'RewardFee', 'WaitingTime', 'Time']
 
 class Model():
@@ -9,11 +10,17 @@ class Model():
     Model object with the different blockchains, the general infos and the model
     :attr model: Pandas dataframe with the scores and values for each blockchaine
     """
-    def __init__(self, dumps=None):
+    def __init__(self, dumps=True, W=None):
         self.blockchains = {}
         self.gen_infos = parserM4.parse_bitinfocharts()
+        if not W:
+            self.W = [0.5, 1, 3]
+        else:
+            self.W = W
         if not dumps:
             self.load_blockchains()
+        else:
+            self.load_dumps()
         self.create_model()
 
     def load_blockchains(self, n_block=5):
@@ -26,10 +33,17 @@ class Model():
             self.blockchains[chain] = parserM4.parse_blockcypher(chain.lower(), n_block=n_block)
             print('%s blockchain parsed!' % chain)
 
+    def load_dumps(self):
+        for crypt in ['BTC', 'ETH', 'LTC', 'DASH', 'DOGE']:
+            file = 'dump_%s.csv' % crypt.lower()
+            p = pathlib.Path(file)
+            if p.is_file():
+                self.blockchains[crypt] = pd.read_csv(file) 
+
     def create_model(self, perc=0.95):
         self.model = pd.DataFrame()
         for chain, df_chain in self.blockchains.items():
-            df_chain[columns[4]] = df_chain[columns[5]].diff(periods=-1).apply(lambda x:x.total_seconds())
+            df_chain[columns[4]] = pd.to_datetime(df_chain[columns[5]]).diff(periods=-1).apply(lambda x:x.total_seconds())
             if chain != 'DOGE':
                 self.model.at['Currency_Market', chain] = self.gen_infos.loc[1][chain]
                 self.model.at['Price_USD', chain] = self.gen_infos.loc[2][chain]
@@ -48,10 +62,9 @@ class Model():
                 self.model.loc[feature] = self.model.loc[feature+'_']/self.model.loc[feature+'_'].max()
             self.model.loc['Cost'] = 1-(self.model.loc['Cost_']/self.model.loc['Cost_'].sum())
 
-            W = [0.5, 1, 3]
-            self.model.loc['Score'] = (W[0]*self.model.loc['Liquidity'] +
-                                       W[1]*self.model.loc['Cost'] + 
-                                       W[2]*self.model.loc['Processing time'])/np.sum(W)
+            self.model.loc['Score'] = (self.W[0]*self.model.loc['Liquidity'] +
+                                       self.W[1]*self.model.loc['Cost'] + 
+                                       self.W[2]*self.model.loc['Processing time'])/np.sum(self.W)
 
 
 
